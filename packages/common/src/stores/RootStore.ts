dc26813d7afb774cf82e70d22fc9bb7fa1d52495
Tommy Chong <tommy.chong@ha.org.hk>
2019-02-26 09:54:15 +0800

    mobx persist

--- packages/common/src/stores/RootStore.ts
+++ packages/common/src/stores/RootStore.ts
@@ -1,10 +1,26 @@
+import { create } from "mobx-persist";
 import { createContext } from "react";
+import { AsyncStorage } from "react-native";
 import { WorkoutStore } from "./WorkoutStore";
 import { WorkoutTimerStore } from "./WorkoutTimerStore";
+
+const hydrate = create({
+  storage: AsyncStorage,
+  jsonify: true
+});
 
 export class RootStore {
   workoutStore = new WorkoutStore(this);
   workoutTimerStore = new WorkoutTimerStore();
+
+  constructor() {
+    hydrate("workout", this.workoutStore);
+    hydrate("workoutTimer", this.workoutTimerStore).then(() => {
+      if (this.workoutTimerStore.isRunning) {
+        this.workoutTimerStore.measure();
+      }
+    });
+  }
 }
 
 export const RootStoreContext = createContext(new RootStore());
